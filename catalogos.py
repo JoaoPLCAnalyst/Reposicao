201e2c5a9debdbc57f0c02a07349cd58e5f1421f
JoaoPLCAnalyst <joao251101@gmail.com>
2026-01-28 10:31:20 -0300

    nova tentativa

--- catalogos.py
+++ catalogos.py
@@ -185,7 +185,7 @@ if cliente_slug:
     with col1:
         if st.button("⬅️ Voltar"):
             # limpa o query param 'cliente' e provoca rerun
-            st.set_query_params()
+            st.set_query_params(cliente=None)
             st.rerun()
     st.subheader("Selecione as peças desejadas abaixo:")
 
